cc5b17e59079d8f7a4bfec3719b4b4d9751cbf43
Abhay Ruparel <abhayruparel2000@gmail.com>
2020-04-19 18:39:22 +0530

    mail: Updates to upload DIFF.txt
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- mail.py
+++ mail.py
@@ -1,16 +1,31 @@
-import os
 import smtplib
+import mimetypes
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
 from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart
 
-user = "#email"
-passwd = "#password"
-
+msg = MIMEMultipart()
+user = "" #Enter sender's email address
+passwd = "" #Enter sender's password
+msg['Subject'] = "Alert! Site has been modified"
+msg['From'] = user
+msg['To'] = "" #Enter receiver's email address
+msgContent = "Hi there,\n\nPlease find attached."
+ctype, encoding = mimetypes.guess_type("DIFF.txt")
+if ctype is None or encoding is not None:
+    ctype = "application/octet-stream"
+Content = MIMEText(msgContent,'plain')
+maintype, subtype = ctype.split("/", 1)
+fp = open("DIFF.txt","rb")
+attachment = MIMEBase(maintype, subtype)
+attachment.set_payload(fp.read())
+encoders.encode_base64(attachment)
+attachment.add_header("Content-Disposition", "attachment", filename="DIFF.txt")
+msg.attach(attachment)
+msg.attach(Content)
 with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-    msg = EmailMessage()
-    msg['Subject'] = "change at site news detected!"
-    msg['From'] = user
-    msg['To'] = 'gamingdynamo121@gmail.com'
-    msg.set_content('Hi there is a change check https://sites.google.com/a/ict.gnu.ac.in/sitenews/home/2020---even-sem ')
     smtp.login(user,passwd)
     smtp.send_message(msg)
-    print ("Done")
+print("Done")
