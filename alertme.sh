c5db4813e1da9921f10728c5654503cf8d5e6281
Arghya Chanda <arghyac35@gmail.com>
2020-08-25 13:08:09 +0530

    Send scrape logs to tg
    Signed-off-by: Arghya Chanda <arghyac35@gmail.com>

--- alertme.sh
+++ alertme.sh
@@ -3,6 +3,8 @@
 #paratmeters
 TODAY=$(TZ=":Asia/Kolkata" date)
 IST_LOCAL=$(TZ=":Asia/Kolkata" date +%A_%F)
+TG_BOT_API_KEY=
+TG_CHAT_ID=
 
 SCRAPE()
 {
@@ -26,6 +28,22 @@ else
 	rm second
 fi
 }
+
+sendLogToTg() {
+	CURL_OPTIONS="-s --form document=@log-$IST_LOCAL.log --form caption=<- --form-string chat_id=$TG_CHAT_ID https://api.telegram.org/bot${TG_BOT_API_KEY}/sendDocument"
+	
+	echo "Executing: curl $CURL_OPTIONS"
+	response=`curl $CURL_OPTIONS <<< "$TEXT"`
+
+	if [[ "$response" != '{"ok":true'* ]]; then
+			echo "Telegram reported an error:"
+			echo $response
+			# If you need yor program to stop if tg returns error, then uncomment the below
+			# echo "Quitting."
+			# exit 1
+	fi
+}
+
 main()
 {
 if [ -f first ]
@@ -43,6 +61,7 @@ fi
  echo -e "\nSecond scrape\n" >> log-$IST_LOCAL.log
  cat second >> log-$IST_LOCAL.log
  echo "==============================================================================" >> log-$IST_LOCAL.log
+sendLogToTg
 COMPARE
 }
 main
