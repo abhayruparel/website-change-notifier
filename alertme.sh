67070c3e6b27ebf4bbe20d385a7e53a057194b28
Abhay Ruparel <abhayruparel2000@gmail.com>
2020-09-19 11:36:10 +0530

    alertme.sh: better indent
    Signed-off-by: Abhay Ruparel <abhayruparel2000@gmail.com>

--- alertme.sh
+++ alertme.sh
@@ -9,47 +9,47 @@ TG_CHAT_ID=$TG_CHAT_ID
 
 SCRAPE()
 {
-python3 scraper.py > $1
+	python3 scraper.py > $1
 }
 COMPARE()
 {
-shaFirst=$(shasum first | awk '{ print $1 }')
-shaSecond=$(shasum second | awk '{ print $1 }')
-if [ "$shaFirst" != "$shaSecond" ]
-then
-	echo "change detected."
-	diff first second > DIFF.txt
-	rm first second
-	echo "Running mail script!"
-	python3 mail.py
-	rm DIFF.txt
-else
-	echo "No change detected"
-	# as there are no changes lets just nuke second.
-	rm second
-fi
+	shaFirst=$(shasum first | awk '{ print $1 }')
+	shaSecond=$(shasum second | awk '{ print $1 }')
+	if [ "$shaFirst" != "$shaSecond" ]
+	then
+		echo "change detected."
+		diff first second > DIFF.txt
+		rm first second
+		echo "Running mail script!"
+		python3 mail.py
+		rm DIFF.txt
+	else
+		echo "No change detected"
+		# as there are no changes lets just nuke second.
+		rm second
+	fi
 }
 
 
 
 main()
 {
-if [ -f first ]
-then
-	SCRAPE second
-else
-	SCRAPE first
-	SCRAPE second
-fi
- # log those first and second html.
- echo "==============================================================================" >> log-$IST_LOCAL.log
- echo $TODAY >> log-$IST_LOCAL.log
- echo -e "First scrape file:\n" >> log-$IST_LOCAL.log
- cat first >> log-$IST_LOCAL.log
- echo -e "\nSecond scrape\n" >> log-$IST_LOCAL.log
- cat second >> log-$IST_LOCAL.log
- echo "==============================================================================" >> log-$IST_LOCAL.log
+	if [ -f first ]
+	then
+		SCRAPE second
+	else
+		SCRAPE first
+		SCRAPE second
+	fi
+	 # log those first and second html.
+	 echo "================================================================" >> log-$IST_LOCAL.log
+	 echo $TODAY >> log-$IST_LOCAL.log
+	 echo -e "First scrape file:\n" >> log-$IST_LOCAL.log
+	 cat first >> log-$IST_LOCAL.log
+	 echo -e "\nSecond scrape\n" >> log-$IST_LOCAL.log
+	 cat second >> log-$IST_LOCAL.log
+	 echo "================================================================" >> log-$IST_LOCAL.log
 
-COMPARE
+	COMPARE
 }
 main
